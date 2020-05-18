336c3bbba62385e8ed465e7b4b1692fb8fe3339d
Lyn Ho <lyn4develop@gmail.com>
2020-05-18 15:58:10 +0800

    toy browser parse attribute

--- week06/toy-browser/parser/index.js
+++ week06/toy-browser/parser/index.js
@@ -4,9 +4,11 @@ const letterReg = /^[a-zA-Z]$/
 const spaceReg = /^[\t\n\f ]$/
 
 let currentToken = {}
+let currentAttribute = {}
 
 function emit(token) {
-  console.log(token)
+  // console.log(token)
+  if(token.type !== 'text') console.log(token)
 }
 
 function data(c) {
@@ -27,9 +29,17 @@ function data(c) {
 function tagOpen(c) {
   if (c === '/') {
     return endTagOpen
-  } else if(c.match(letterReg)) {
+  } else if (c.match(letterReg)) {
+    currentToken = {
+      type: 'startTag',
+      tagName: ''
+    }
     return tagName(c)
   } else {
+    emit({
+      type: 'text',
+      content: c
+    })
     return
   }
 }
@@ -62,6 +72,7 @@ function tagName() {
     emit(currentToken)
     return data
   } else {
+    currentToken.tagName += c
     return tagName
   }
 }
@@ -69,12 +80,145 @@ function tagName() {
 function beforeAttributeName(c) {
   if (c.match(spaceReg)) {
     return beforeAttributeName
-  } else if (c === '>') {
-    return data
+  } else if (c === '/' || c === '>' || c === EOF) {
+    return afterAttributeName(c)
   } else if (c === '=') {
-    return beforeAttributeName
+    
   } else {
+    currentAttribute = {
+      name: '',
+      value: ''
+    }
+
+    return attributeName(c)
+  }
+}
+
+function afterAttributeName(c) {
+  if (c.match(spaceReg)) {
+    return afterAttributeName
+  } else if (c === '/') {
+    return selfClosingStartTag
+  } else if (c === '=') {
+    return beforeAttributeValue
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+
+    emit(currentToken)
+
+    return data
+  } else if (c === EOF) {
+    
+  } else {
+    currentToken[currentAttribute.name] = currentAttribute.value
+
+    currentAttribute = {
+      name: '',
+      value: ''
+    }
+
+    return attributeName(c)
+  }
+}
+
+function attributeName(c) {
+  if (c.match(spaceReg) || c === '/' || c === '>' || c === EOF) {
+    return afterAttributeName(c)
+  } else if (c === '=') {
+    return beforeAttributeValue
+  } else if (c === '\u0000') {
+    
+  } else if (c === '"' || c === '\'' || c === '<') {
+    
+  } else {
+    currentAttribute.name += c
+    return attributeName
+  }
+}
+
+function beforeAttributeValue(c) {
+  if (c.match(spaceReg) || c === '/' || c === '>' || c === EOF) {
+    return beforeAttributeValue
+  } else if (c === '"') {
+    return doubleQuotedAttributeValue
+  } else if (c === '\'') {
+    return singleQuotedAttributeValue
+  } else if (c === '>') {
+    
+  } else {
+    return unquotedAttributeValue(c)
+  }
+}
+
+function doubleQuotedAttributeValue(c) {
+  if (c === '"') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return afterQuotedAttributeValue
+  } else if (c === '\u0000') {
+    
+  } else if (c === EOF) {
+    
+  } else {
+    currentAttribute.value += c
+    return doubleQuotedAttributeValue
+  }
+}
+
+function singleQuotedAttributeValue(c) {
+  if (c === '\'') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return afterQuotedAttributeValue
+  } else if (c === '\u0000') {
+    
+  } else if (c === EOF) {
+    
+  } else {
+    currentAttribute.value += c
+    return singleQuotedAttributeValue
+  }
+}
+
+function afterQuotedAttributeValue(c) {
+  if (c.match(spaceReg)) {
     return beforeAttributeName
+  } else if (c === '/') {
+    return selfClosingStartTag
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+
+    emit(currentToken)
+
+    return data
+  } else if (c === EOF) {
+    
+  } else {
+    currentAttribute.value += c
+    return afterQuotedAttributeValue  // diff doubleQuotedAttributeValue
+  }
+}
+
+function unquotedAttributeValue(c) {
+  if (c.match(spaceReg)) {
+    currentToken[currentAttribute.name] = currentAttribute.value
+
+    return beforeAttributeName
+  } else if (c === '/') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+
+    return selfClosingStartTag
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    emit(currentToken)
+    return data
+  } else if (c === '\u0000') {
+    
+  } else if (c === '"' || c === '\'' || c === '<' || c === '=' || c === '`') {
+    
+  } else if (c === EOF) {
+    
+  } else {
+    currentAttribute.value += c
+    return unquotedAttributeValue
   }
 }
 
